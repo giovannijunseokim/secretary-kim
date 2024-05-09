1a67028f606e211b346f894d68f2c564fbacdf47
giovannijunseokim <kingjunseo@gmail.com>
2024-05-09 20:16:22 +0900

    add resources

---
+++ lib/theme/colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+final class SkColors{
+  static Color black =  const Color(0xFF1F1F23);
+
+  static Color white = const Color(0xFFFFFFFF);
+
+  static Color  gray1 = const Color(0xFF696E72);
+  static Color  gray2 =const  Color(0xFF818992);
+  static Color  gray3 = const Color(0xFFE7E7E8);
+
+  static Color  blue1 = const Color(0xFF2D74FF);
+  static Color  blue2 = const Color(0xFF738BAB);
+
+  static Color  red1 = const Color(0xFFD84041);
+  static Color  red2 =const  Color(0xFFEC6B6B);
+}
