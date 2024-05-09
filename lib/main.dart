215945be29cb391229629e8f668e01a8a0cc2adf
giovannijunseokim <kingjunseo@gmail.com>
2024-05-09 20:16:48 +0900

    implement ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:secretary_kim/theme/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,115 +13,169 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        backgroundColor: SkColors.black,
+        body: const Column(
+          children: [
+            // For Status Bar
+            SizedBox(height: 24),
+            TopBar(),
+            SizedBox(height: 14),
+            Title(title: "준비된 소지품을 클릭해주세요"),
+            SizedBox(height: 34),
+            BelongingsGrid(),
+            MoveButton(isEnabled: false)
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class TopBar extends StatelessWidget {
+  const TopBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+    return Column(
+      children: [
+        const SizedBox(height: 30),
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(width: 28),
+            SvgPicture.asset(
+              "assets/icons/ic_arrow_left.svg",
+              width: 24,
+              height: 24,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const Spacer(),
+            Image.asset(
+              "assets/images/img_secretary_kim.png",
+              width: 98,
+              height: 119,
             ),
+            const Spacer(),
+            SvgPicture.asset(
+              "assets/icons/ic_modify.svg",
+              width: 24,
+              height: 24,
+            ),
+            const SizedBox(width: 28),
           ],
-        ),
+        )
+      ],
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  final String title;
+
+  const Title({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(
+        color: SkColors.white,
+        fontSize: 18,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class BelongingsGrid extends StatelessWidget {
+  const BelongingsGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 24,
+        childAspectRatio: 140 / 81,
+        padding: const EdgeInsets.symmetric(horizontal: 28),
+        children: const [
+          BelongingCard(name: "휴대폰", isClicked: true),
+          BelongingCard(name: "지갑", isClicked: false),
+          BelongingCard(name: "휴대폰", isClicked: false),
+          BelongingCard(name: "지갑", isClicked: false),
+          AddBelongingCard()
+        ],
+      ),
+    );
+  }
+}
+
+class BelongingCard extends StatelessWidget {
+  final String name;
+  final bool isClicked;
+
+  const BelongingCard({super.key, required this.name, required this.isClicked});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: isClicked ? SkColors.blue1 : SkColors.gray2,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        name,
+        style: TextStyle(color: SkColors.white, fontSize: 18),
+      ),
+    );
+  }
+}
+
+class AddBelongingCard extends StatelessWidget {
+  const AddBelongingCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: SkColors.blue2,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: SvgPicture.asset(
+        "assets/icons/ic_plus.svg",
+        color: SkColors.blue2,
+        width: 24,
+        height: 24,
+      ),
+    );
+  }
+}
+
+class MoveButton extends StatelessWidget {
+  final bool isEnabled;
+
+  const MoveButton({super.key, required this.isEnabled});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 28),
+      padding: const EdgeInsets.symmetric(vertical: 15),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: isEnabled ? SkColors.blue1 : SkColors.gray2,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        "이동하기",
+        style: TextStyle(color: SkColors.white, fontSize: 18),
+      ),
     );
   }
 }
