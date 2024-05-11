3948cbb5f1d5e2a4f982ba645b8b8c28eacc3c2d
giovannijunseokim <kingjunseo@gmail.com>
2024-05-11 22:29:33 +0900

    make const

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,17 +1,17 @@
 import 'dart:ui';
 
 final class SkColors {
-  static Color black = const Color(0xFF1F1F23);
+  static const Color black = Color(0xFF1F1F23);
 
-  static Color white = const Color(0xFFFFFFFF);
+  static const Color white = Color(0xFFFFFFFF);
 
-  static Color gray1 = const Color(0xFF696E72);
-  static Color gray2 = const Color(0xFF818992);
-  static Color gray3 = const Color(0xFFE7E7E8);
+  static const Color gray1 = Color(0xFF696E72);
+  static const Color gray2 = Color(0xFF818992);
+  static const Color gray3 = Color(0xFFE7E7E8);
 
-  static Color blue1 = const Color(0xFF2D74FF);
-  static Color blue2 = const Color(0xFF738BAB);
+  static const Color blue1 = Color(0xFF2D74FF);
+  static const Color blue2 = Color(0xFF738BAB);
 
-  static Color red1 = const Color(0xFFD84041);
-  static Color red2 = const Color(0xFFEC6B6B);
+  static const Color red1 = Color(0xFFD84041);
+  static const Color red2 = Color(0xFFEC6B6B);
 }
